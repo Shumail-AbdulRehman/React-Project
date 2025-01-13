96c44d6fe335e9f22a2283a3fad1af7952cbb393
SHUMAIL <47891@students.riphah.edu.pk>
2025-01-13 14:55:15 +0500

    Update API.js

--- src/API.js
+++ src/API.js
@@ -1,4 +1,4 @@
-// const URI = 'http://localhost:8080';
-const URI = 'https://danish-backend-evheghf5akhma6ez.canadacentral-01.azurewebsites.net/';
+const URI = 'http://localhost:8080';
+// const URI = 'https://danish-backend-evheghf5akhma6ez.canadacentral-01.azurewebsites.net/';
 export const BASE_URL = URI ;
 export const AUTH = `${BASE_URL}/auth`;
